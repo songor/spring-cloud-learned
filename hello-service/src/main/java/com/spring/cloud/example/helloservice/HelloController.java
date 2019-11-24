502cbdaeb22f95589d0ad629a779e043c33dc65a
songor <38952103+songor@users.noreply.github.com>
2019-11-24 18:31:26 +0800

    删除被注释的内容

--- hello-service/src/main/java/com/spring/cloud/example/helloservice/HelloController.java
+++ hello-service/src/main/java/com/spring/cloud/example/helloservice/HelloController.java
@@ -1,25 +1,15 @@
 package com.spring.cloud.example.helloservice;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Slf4j
 @RestController
 public class HelloController {
 
-//    @Autowired
-//    private DiscoveryClient client;
-
     @GetMapping("/hello")
     public String hello() {
-//        List<ServiceInstance> instances = client.getInstances("hello-service");
-//        instances.stream().forEach((instance) -> log.info("/hello, host: " + instance.getHost() + ", port: " + instance.getPort() + ", service id: " + instance.getServiceId()));
         log.info("Received request");
         return "Hello World";
     }
