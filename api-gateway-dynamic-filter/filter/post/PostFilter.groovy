ad72ab233663e8997adf2bb95c755f145ec24c35
songor <38952103+songor@users.noreply.github.com>
2019-12-05 17:13:24 +0800

    去除不必要的 import

--- api-gateway-dynamic-filter/filter/post/PostFilter.groovy
+++ api-gateway-dynamic-filter/filter/post/PostFilter.groovy
@@ -1,6 +1,5 @@
 import com.netflix.zuul.ZuulFilter
 import com.netflix.zuul.context.RequestContext
-import com.netflix.zuul.exception.ZuulException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
